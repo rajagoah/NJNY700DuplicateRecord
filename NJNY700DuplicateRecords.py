83fb778817c091e82756263c56040ca1443a7c02
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-03-30 16:30:22 -0400

    Incoming first updates
    This again was an amazon interview question. While I was able to solve this using SQL, I am attempting to solve this using Python

---
+++ NJNY700DuplicateRecords.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv("/Users/aakarsh.rajagopalan/Personal documents/Python projects/NJNY700DuplicateRecords/NJNY700DuplicateRecords.csv")
+ind_c1 = []
+ind_c2 = []
+
+for i in range(len(df)):
+    if df["C1"][i:i+1].to_list()[0] == 'NJ' or df["C1"][i:i+1].to_list()[0] == 'NY':
+        ind_c1.append(1)
+    else:
+        ind_c1.append(0)
+
+df["ind_c1"]=ind_c1
+
+print(df.loc[df["ind_c1"] == '1'])
+
